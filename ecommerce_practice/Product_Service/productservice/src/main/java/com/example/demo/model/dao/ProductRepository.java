dcc7a51afabadbd04903ab607fdacdcf0ce25e71
AlierT <tetsu.kala@gmail.com>
2025-03-09 12:50:51 +0800

    3rd commit

--- ecommerce_practice/Product_Service/productservice/src/main/java/com/example/demo/model/dao/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.model.dao;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.dao.entity.ProductEntity;
-
-public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-}
